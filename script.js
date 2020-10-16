781402d7bed6c07621672d46ebead993e005e907
paolorafaelyap <paolorafaelyap@gmail.com>
2020-10-16 01:46:15 -0700

    added variables for criteria

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+
+// first, create arrays for each criteria
+
+var lowerCase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", 
+"J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", 
+"Y", "Z"];
+
+var numeric=["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var specialChar=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">","?"];
+
+// if desired password contains all criteria
+
+var all = lowerCase.concat(upperCase, numeric, specialChar);
+
+//if desired password only contains three of the criteria
+
+var minusLowerCase = upperCase.concat(numeric, specialChar);
+var minusUpperCase = lowerCase.concat(numeric, specialChar);
+var minusNumeric = lowerCase.concat(upperCase, specialChar);
+var minusSpecial= lowerCase.concat(upperCase, numeric);
+
+//if desired password contains two criteria
+var letters = [].concat(lowerCase, upperCase);
+var numLower=[].concat(lowerCase, numeric);
+var specLower=[].concat(lowerCase, specialChar);
+var numUpper=[].concat(upperCase, numeric);
+var specUpper=[].concat(upperCase, specialChar);
+var specNum=[].concat(numeric, specialChar);
+
+//next, create a variable that randomizes
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
